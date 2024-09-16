db0d0ae4fee5b0b124591d8869d05099c186df39
Gabriela  Freitas <gabrielafreitas@Gabrielas-Air-2.lan>
2024-09-16 22:03:54 +0100

    init mariadb

---
+++ srcs/requirements/mariadb/config/mdb-config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#--------------mariadb start--------------#
+service mariadb start 
+sleep 5 
+
+#--------------mariadb config--------------#
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_HOST}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
+mariadb -e "FLUSH PRIVILEGES;"
+
+#--------------mariadb restart--------------#
+# Shutdown mariadb to restart with new config
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
+# Restart mariadb with new config in the background to keep the container running
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
