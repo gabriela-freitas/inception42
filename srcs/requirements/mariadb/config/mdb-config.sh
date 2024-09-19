3f5983406c6cd0fe925f27db9acdfa2d5e4eaad8
gabrielafreitas <gabriela>
2024-09-19 19:29:34 +0100

    workin

--- srcs/requirements/mariadb/config/mdb-config.sh
+++ srcs/requirements/mariadb/config/mdb-config.sh
@@ -6,9 +6,9 @@ sleep 5
 
 #--------------mariadb config--------------#
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_HOST}\`;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON ${DB_HOST}.* TO \`${MYSQL_USER}\`@'%';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO \`${MYSQL_USER}\`@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
 
 #--------------mariadb restart--------------#
--- srcs/requirements/wordpress/config/wp_conf.sh
+++ srcs/requirements/wordpress/config/wp_conf.sh
@@ -40,7 +40,7 @@ fi
 # download wordpress core files
 wp core download --allow-root
 # create wp-config.php file with database details
-wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
+wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
 # install wordpress with the given title, admin username, password and email
 wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
 #create a new user with the given username, email, password and role
@@ -49,7 +49,7 @@ wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U
 #---------------------------------------------------php config---------------------------------------------------#
 
 # change listen port from unix socket to 9000
-sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i '36 s@/run/php/php7.4-fpm.sock@wordpress:9000@' /etc/php/7.4/fpm/pool.d/www.conf
 # create a directory for php-fpm
 mkdir -p /run/php
 # start php-fpm service in the foreground to keep the container running
